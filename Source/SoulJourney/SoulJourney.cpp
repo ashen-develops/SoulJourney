363c9aaed53aa8e45bb1d689b8d279e3539f48b7
Ashen Pennington <69054738+ashen-develops@users.noreply.github.com>
2023-12-01 19:22:42 -0600

    Initial commit

---
+++ Source/SoulJourney/SoulJourney.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SoulJourney.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SoulJourney, "SoulJourney" );
